faa8f7905b9c5b60abb6376e64dca8cfc18089ec
keshav ashta <kashta@greenapplesolutions.com>
2013-03-30 14:57:35 +0530

    updated edit enquiry service, and added password label in login view

--- trunk/app/controllers/HomeController.php
+++ trunk/app/controllers/HomeController.php
@@ -1,19 +1,20 @@
 <?php
 
-class HomeController extends BaseController {
+class HomeController extends BaseController
+{
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Default Home Controller
+    |--------------------------------------------------------------------------
+    |
+    | You may wish to use controllers instead of, or in addition to, Closure
+    | based routes. That's great! Here is an example controller method to
+    | get you started. To route to this controller, just add the route:
+    |
+    |	Route::get('/', 'HomeController@showWelcome');
+    |
+    */
 
     public function __construct()
     {
@@ -25,4 +26,5 @@ class HomeController extends BaseController {
     {
         return View::make('home');
     }
+
 }
